744926e6765177780f6a3d43136d0db0396ef7af
ja8eer <ja8eer@localhost.localdomain>
2020-09-28 20:41:30 +0000

    animated a little

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -3,8 +3,17 @@ package com.example.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void drop(View view){
+        ImageView counter = (ImageView) view;
+        counter.setTranslationY(-1000f);
+        counter.setImageResource(R.drawable.yellowbtn1);
+        counter.animate().translationYBy(1000f).setDuration(300);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
