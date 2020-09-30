fe0a19fba8bc6eff8fc7f606c7af1a1f4fc85289
ja8eer <ja8eer@localhost.localdomain>
2020-09-30 01:21:59 +0000

    added a few codes

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -5,14 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextClock;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     // 0 for yellow and 1 for red
     int ActivePlayer = 0;
     int[] gameState = {2,2,2,2,2,2,2,2,2};
+    int[][] Winners = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
 
 
-    public void drop(View view){
+    public void drop(View view) {
         ImageView counter = (ImageView) view;
         int tappedCounter = Integer.parseInt(counter.getTag().toString());
         if (gameState[tappedCounter] == 2) {
@@ -27,6 +31,22 @@ public class MainActivity extends AppCompatActivity {
             }
 
             counter.animate().translationYBy(1000f).setDuration(300);
+            for (int[] winner : Winners) {
+                if (gameState[winner[0]] == gameState[winner[1]] &&
+                        gameState[winner[1]] == gameState[winner[2]] &&
+                        gameState[winner[0]] != 2) {
+                    String win = "Red";
+                    if (gameState[winner[0]] == 0) {
+                        win = "Yellow";
+                    }
+                    System.out.println("0" + gameState[winner[0]]);
+                    System.out.println("1" +gameState[winner[1]]);
+                    TextView winnerMsg = (TextView) findViewById(R.id.winnerMsg);
+                    winnerMsg.setText(win + " has won");
+                    LinearLayout playAgainLay = (LinearLayout) findViewById(R.id.plyAgainlay);
+                    playAgainLay.setVisibility(View.VISIBLE);
+                }
+            }
         }
     }
 
